e5aeed0bf0c1b7d34a6017e9562314dc829c53ab
triwahyuprasetyo <tri.wahyu@sebangsa.com>
2017-01-18 14:30:24 +0700

    multi object

--- app/src/main/java/com/triwahyuprasetyo/appfirebase/MainActivity.java
+++ app/src/main/java/com/triwahyuprasetyo/appfirebase/MainActivity.java
@@ -4,27 +4,41 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseReference mRef;
-    private Button buttonAddValue;
+    private DatabaseReference mRef;
+    private Button buttonAddValue1,buttonAddValue2;
+    private EditText editTextValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://appfirebase-2fd28.firebaseio.com/");
-        buttonAddValue = (Button) findViewById(R.id.btAddVal);
 
-        buttonAddValue.setOnClickListener(new View.OnClickListener() {
+        editTextValue= (EditText) findViewById(R.id.editTextValue);
+        buttonAddValue1 = (Button) findViewById(R.id.btAddVal);
+        buttonAddValue1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://appfirebase-2fd28.firebaseio.com");
+                String value=editTextValue.getText().toString();
                 DatabaseReference dbR = mRef.child("Name");
-                dbR.setValue("Wahyu");
+                dbR.setValue(value);
+            }
+        });
+        buttonAddValue2 = (Button) findViewById(R.id.btAddVal2);
+        buttonAddValue2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://appfirebase-2fd28.firebaseio.com/Users");
+                String value=editTextValue.getText().toString();
+                DatabaseReference dbR = mRef.child("Age");
+                dbR.setValue(value);
             }
         });
     }
